356dde81f1bdc6362be4c59729da17ed45119e68
Mohamed Fathy <mohamedahmedfathy1995@gmail.com>
2021-08-23 17:21:41 +0200

    add nav and sort list

---
+++ src/app/Components/sort-dropdown/sort-dropdown.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-sort-dropdown',
+  templateUrl: './sort-dropdown.component.html',
+  styleUrls: ['./sort-dropdown.component.scss']
+})
+export class SortDropdownComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { NavComponent } from './Components/nav/nav.component';
+import { SearchBarComponent } from './Components/search-bar/search-bar.component';
+import { HomeComponent } from './Components/home/home.component';
+import { SortDropdownComponent } from './Components/sort-dropdown/sort-dropdown.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent,
+    SearchBarComponent,
+    HomeComponent,
+    SortDropdownComponent
   ],
   imports: [
     BrowserModule
